8fac3b31e1e534d3fe250a2df93e9aa95284b625
Firtecy <admin@firtecy.de>
2014-08-26 15:24:44 +0200

    arm: kernel: traps GCC 4.9.x fix up
    Change-Id: I149490db76f1cf6b672d94ed55864664e48d4621

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -272,6 +272,9 @@ void die(const char *str, struct pt_regs *regs, int err)
 {
 	struct thread_info *thread = current_thread_info();
 	int ret;
+#if defined(CONFIG_MACH_LGE) && defined(CONFIG_MACH_STAR)
+	unsigned char bootcause[1];
+#endif
 
 	oops_enter();
 
@@ -289,8 +292,8 @@ void die(const char *str, struct pt_regs *regs, int err)
 	add_taint(TAINT_DIE);
 	spin_unlock_irq(&die_lock);
 #if defined(CONFIG_MACH_LGE) && defined(CONFIG_MACH_STAR)
-	unsigned char bootcause[1] = {LGE_NVDATA_RESET_CAUSE_VAL_USER_RESET};
-	lge_nvdata_write(LGE_NVDATA_RESET_CAUSE_OFFSET, bootcause,1);
+	bootcause[0] = LGE_NVDATA_RESET_CAUSE_VAL_USER_RESET;
+	lge_nvdata_write(LGE_NVDATA_RESET_CAUSE_OFFSET, bootcause, 1);
 #endif
 	oops_exit();
 
